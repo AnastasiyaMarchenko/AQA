ec86d21d57ed3850a72ec71fc77cf95f8f66de7a
anastasiya_m <marchenkoanastasiia21@gmail.com>
2019-10-19 21:51:03 +0300

    task 1 is done

--- src/com/company/HomeWork/Task1_6.java
+++ src/com/company/HomeWork/Task1_6.java
@@ -8,14 +8,15 @@ public class Task1_6 {
 
     public static void main(String[] args) {
 
-        Scanner n = new Scanner(System.in);
-        int array[] = {333, 6575, 777, 9866, 545};
-/*        int array[] = new int[5];
+/*        Scanner n = new Scanner(System.in);
+        // int array[] = {333, 657, 777, 666, 565};
+        int array[] = new int[5];
         for (int i = 0; i < array.length; i++) {
             array[i] = n.nextInt();
         }*/
 
-       /* Четные и нечетные числа
+/*   //Четные и нечетные числа
+
         for (int i : array) {
             if (i % 2 == 0) {
                 System.out.println("Число четное  " + i);
@@ -25,7 +26,8 @@ public class Task1_6 {
         }*/
 
 
-/*       //Наибольшее и наименьшее число
+/*   //Наибольшее и наименьшее число
+
         int min = array[0];
         int max = array[0];
 
@@ -40,7 +42,8 @@ public class Task1_6 {
         System.out.println("Максимальное значение  " + max + "  Минимальное значение  " + min);
     }*/
 
-/*        //Числа которые делятся на 3 или на 9
+/*   //Числа которые делятся на 3 или на 9
+
         for (int i : array) {
             if (i % 3 == 0) {
                 System.out.println("Число кратно 3м  " + i);
@@ -52,21 +55,48 @@ public class Task1_6 {
         }*/
 
 
-/*          //Числа которые делятся на 5 и на 7
+/*   //Числа которые делятся на 5 и на 7
+
         for (int i : array) {
             if (i % 5 == 0 && i % 7 == 0) {
                 System.out.println("Число делится на 5 и на 7  " + i);
             } else {
                 System.out.println("Число не делится на 5 и на 7  " + i);
             }
-    }
-    */
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                
-            }
-        }
+    }*/
 
+/*// Все трехзначные числа в записи которых нет одинаковых цифр
+
+        for (int i = 0; i < array.length; i++) {
+            String tmp = String.valueOf(array[i]);
+            int itemLength = tmp.length();
+            if (itemLength == 3) {
+                char firstChar = tmp.charAt(0);
+                char secondChar = tmp.charAt(1);
+                char thirdChar = tmp.charAt(2);
+
+                if (firstChar != secondChar && firstChar != thirdChar && secondChar != thirdChar) {
+                    System.out.println(tmp);
+                }
+            }
+        }*/
+
+/*    //"Счастливые"числа
+
+        for (int i = 0; i < array.length; i++) {
+            String tmp = String.valueOf(array[i]);
+            int itemLength = tmp.length();
+            if (itemLength == 4) {
+                char firstChar = tmp.charAt(0);
+                char secondChar = tmp.charAt(1);
+                char thirdChar = tmp.charAt(2);
+                char fourthChar = tmp.charAt(3);
+
+                if (firstChar + secondChar == thirdChar + fourthChar) {
+                    System.out.println(tmp);
+                }
+            }
+        }*/
 
     }
 }
